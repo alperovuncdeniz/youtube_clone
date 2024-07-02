fe8780f0c203280e8ecf375f6774caf9ddfb2c63
Alper Övünç Deniz <alperovuncdeniz@gmail.com>
2024-07-02 15:27:57 +0300

    a

--- lib/features/auth/pages/login_page.dart
+++ lib/features/auth/pages/login_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:youtube_clone/features/auth/repository/auth_service.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends ConsumerWidget {
   const LoginPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext contex, WidgetRef ref) {
     return Scaffold(
       body: SafeArea(
         child: Center(
@@ -32,7 +34,9 @@ class LoginPage extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(bottom: 55),
                 child: GestureDetector(
-                  onTap: () {},
+                  onTap: () async {
+                    await ref.read(authServiceProvider).signInWithGoogle();
+                  },
                   child: Image.asset(
                     "assets/images/signinwithgoogle.png",
                     height: 60,
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:youtube_clone/features/auth/login_page.dart';
+import 'package:youtube_clone/features/auth/pages/login_page.dart';
 import 'package:youtube_clone/firebase_options.dart';
 
 void main() async {
@@ -10,16 +11,24 @@ void main() async {
   runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Youtube Clone',
-      home: LoginPage(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return LoginPage();
+          }
+          return Container();
+        },
+      ),
     );
   }
 }
