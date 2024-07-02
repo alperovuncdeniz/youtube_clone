c265352e72f624c74d0c874d6892e524c7e06dac
Alper Övünç Deniz <alperovuncdeniz@gmail.com>
2024-07-02 12:54:17 +0300

    a

--- lib/features/auth/login_page.dart
+++ lib/features/auth/login_page.dart
@@ -6,7 +6,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Center(
           child: Column(
